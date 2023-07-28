2478f502099ae1cacbf0e718dadeec1fe7f3b0a6
JoaqinDLH <134020388+JoaqinDLH@users.noreply.github.com>
2023-07-27 20:57:40 -0400

    ok

---
+++ src/main/java/Servlets/Contacto.java
@@ -0,0 +1,60 @@
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class Contacto extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Contacto() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("loggedIn") == null) {
+
+            response.sendRedirect("iniciar-sesion");
+        } else {
+            request.setAttribute("seccion", "contacto");
+            getServletContext().getRequestDispatcher("/views/contacto.jsp").forward(request, response);
+        }
+
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        String nombre = request.getParameter("nombre");
+        request.setAttribute("respuesta", "<div class=\"alert alert-success alert-dismissible fade show\" role=\"alert\" id=\"envio_exitoso\" style=\"display: none;\">\n" +
+                "      <p id=\"message\">" + nombre + " tu formulario ya ha sido enviado , nos contactaremos a la brevedad" + "</p>\n" +
+                "      <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>\n" +
+                "    </div>");
+        //response.getWriter().write(");
+        PrintWriter out = response.getWriter();
+        out.println("Información del contacto :");
+        out.println("Nombre : " + nombre);
+        out.println("Correo :" + request.getParameter("email"));
+        out.println("Detalle : " + request.getParameter("detalle"));
+
+
+        RequestDispatcher rd = request.getRequestDispatcher("/views/contacto.jsp");
+        rd.forward(request, response);
+    }
+
+}
+
---
+++ src/main/java/Servlets/Inicio.java
@@ -0,0 +1,20 @@
+package Servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+public class InicioServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Página de inicio</title></head>");
+        out.println("<body>");
+        out.println("<h1>Bienvenido a mi página de inicio</h1>");
+        out.println("<p>Este es un servlet de inicio en funcionamiento.</p>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
