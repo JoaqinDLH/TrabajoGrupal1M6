6a26c673751ce720c088d7108d1747883040d897
JoaqinDLH <134020388+JoaqinDLH@users.noreply.github.com>
2023-08-10 20:31:06 -0400

    Changes by vitox

--- src/main/java/Servlets/Contacto.java
+++ src/main/java/Servlets/Contacto.java
@@ -2,8 +2,15 @@ package Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-
+@WebServlet(name="contacto" , value="/contacto")
 public class Contacto extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -57,4 +64,3 @@ public class Contacto extends HttpServlet {
     }
 
 }
-
--- src/main/java/Servlets/Inicio.java
+++ src/main/java/Servlets/Inicio.java
@@ -5,7 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-public class InicioServlet extends HttpServlet {
+
+public class Inicio extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
